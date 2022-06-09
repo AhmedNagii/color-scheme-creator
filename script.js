240751e9c349815c1d6f6b77c9f2e11272ecf8af
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-06-09 17:58:14 +0200

    create the colors array from the api

--- script.js
+++ script.js
@@ -7,7 +7,17 @@
 
 
 document.getElementById('get-scheme').addEventListener('click', () => {
-    const selectedColor = document.getElementById('color-selector').value
-
-    console.log(selectedColor)
+    const selectedColor = document.getElementById('color-selector').value.substring(1);
+    let schemeArray = []
+    selectedColor
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=analogic&count=5`)
+    .then(res => res.json())
+    .then(data => {
+        for(let i =0; i < 5; i++){
+        let color = data.colors[i].hex.value
+        schemeArray.push(color)
+}
+console.log(schemeArray)} )
+    
 })
+
