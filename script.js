36b39db2ca15f2f1c6a2e38f54fd94b496b4b4d6
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-06-10 16:27:52 +0200

    Functionally work
    the app has all the features copyable text and getting scheme from the api

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
+const colorsText = document.getElementsByClassName("color-code");
+const getSchemeBtn = document.getElementById("get-scheme");
+const colorTextCodeEl = document.getElementsByClassName("color-code");
+const snackBar = document.getElementById("snackbar");
 
+getSchemeBtn.addEventListener("click", getNewScheme);
 
+function makeTextCopyable() {
+  for (let i = 0; i < colorTextCodeEl.length; i++) {
+    colorTextCodeEl[i].onclick = () => {
+      document.execCommand("copy");
+    };
+    colorTextCodeEl[i].addEventListener("copy", (event) => {
+      event.preventDefault();
 
+      snackBar.className = "show";
 
-
-
-
-
-document.getElementById('get-scheme').addEventListener('click', () => {
-    const selectedColor = document.getElementById('color-selector').value.substring(1);
-    let schemeArray = []
-    selectedColor
-    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=analogic&count=5`)
-    .then(res => res.json())
-    .then(data => {
-        for(let i =0; i < 5; i++){
-        let color = data.colors[i].hex.value
-        schemeArray.push(color)
+      setTimeout(() => {
+        snackBar.className = snackBar.className.replace("show", "snackbar");
+      }, 3000);
+      if (event.clipboardData) {
+        event.clipboardData.setData(
+          "text/plain",
+          colorTextCodeEl[i].textContent
+        );
+      }
+    });
+  }
 }
-console.log(schemeArray)} )
-    
-})
 
+function getNewScheme() {
+  getSchemeBtn.disabled = true;
+  const selectedColor = document
+    .getElementById("color-selector")
+    .value.substring(1);
+
+  const selectedMode = document.getElementById("mode").value.toLowerCase();
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < 5; i++) {
+        let color = data.colors[i].hex.value;
+        colorsText[i].textContent = color;
+        document.getElementsByClassName("single-color-container")[
+          i
+        ].style.backgroundColor = color;
+      }
+    });
+  getSchemeBtn.disabled = false;
+}
+
+getNewScheme();
+makeTextCopyable();
